3c842ac3f88f50576bf5c626db715bfa99b73706
Thrusher <devilslip@gmail.com>
2014-11-21 09:34:19 +0100

    init

---
+++ Health Hackathon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Health Hackathon
+//
+//  Created by Patryk Drozd on 21.11.2014.
+//  Copyright (c) 2014 Patryk Drozd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Health HackathonTests/Health_HackathonTests.m
@@ -0,0 +1,40 @@
+//
+//  Health_HackathonTests.m
+//  Health HackathonTests
+//
+//  Created by Patryk Drozd on 21.11.2014.
+//  Copyright (c) 2014 Patryk Drozd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Health_HackathonTests : XCTestCase
+
+@end
+
+@implementation Health_HackathonTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
